120d051787f2666c02dfbb37ea3f535f8c6fd716
nikaidowataru <nnn.www.0319@gmail.com>
2020-01-15 21:59:58 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  # devise_for :users, controllers: {
+  #   registrations: 'users/registrations',
+  # }
+  # devise_scope :user do
+  #   get 'addresses', to: 'users/registrations#new_register'
+  #   post 'addresses', to: 'users/registrations#create_register'
+  # end
+
   root 'post#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
